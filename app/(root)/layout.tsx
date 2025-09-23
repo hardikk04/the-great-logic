48c26a00e3711e144003fd0a63fe69a3cf910e14
Hardik Sisodiya <hardik04062003@gmail.com>
2025-09-24 00:45:05 +0530

    removed lenis

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
-import SmoothScroll from "@/components/shared/SmoothScroll";
 gsap.registerPlugin(ScrollTrigger);
 
 const geistSans = Geist({
@@ -31,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SmoothScroll>{children}</SmoothScroll>
+        {children}
       </body>
     </html>
   );
